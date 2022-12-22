c3f3aa5ee6c501c5bdf07eab76b4e11135b1d249
Kieran Emery <kizwiz@hotmail.co.uk>
2022-12-22 23:35:52 +0000

    Add project files.

---
+++ ChuckNorrisJokeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisApp
+{
+    public class ChuckNorrisJokeService : IJokeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ChuckNorrisJokeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetRandomJokeAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://api.chucknorris.io/jokes/random");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                dynamic joke = Newtonsoft.Json.JsonConvert.DeserializeObject(responseContent);
+                return joke.value;
+            }
+            else
+            {
+                throw new Exception($"Request failed with status code {response.StatusCode}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ChuckNorrisApp;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create an HttpClient instance
+        HttpClient httpClient = new HttpClient();
+
+        // Create an instance of the ChuckNorrisJokeService
+        IJokeService jokeService = new ChuckNorrisJokeService(httpClient);
+
+        // Get a random joke and print it to the console
+        string joke = await jokeService.GetRandomJokeAsync();
+        Console.WriteLine(joke);
+    }
+}
